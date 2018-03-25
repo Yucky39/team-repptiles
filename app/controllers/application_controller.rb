4bd13ef0e5316b7b06b068cc6205568e3f4a8196
takeuchi <takeuchi.hrt@gmail.com>
2018-03-25 04:43:08 +0000

    genjou

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
-    devise_parameter_sanitizer.for(:sign_up)<< [:name, :user_image]
   end
 end
